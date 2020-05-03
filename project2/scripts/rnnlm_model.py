b5f2d8faacd1b1b6b6a532df5dc90ce4a2f65065
lennertjansen <lennertjansen95@gmail.com>
2020-05-03 15:03:20 +0200

    Created directory for project 2.

---
+++ project2/scripts/rnnlm_model.py
@@ -0,0 +1,40 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Set the random seed manually for reproducibility.
+torch.manual_seed(2020)
+
+# Check if GPU available
+is_gpu_available = torch.cuda.is_available()
+
+if is_gpu_available:
+    device = torch.device("cuda")
+    print('GPU is available.')
+else:
+    device = torch.device('cpu')
+    print("GPU not available, CPU used instead.")
+
+class RNNLM(nn.Module):
+    """Container module for RNN Language Model. Consists of an encoder,
+    a recurrent module, and a decoder."""
+
+    def __init__(self, rnn_type = 'GRU', ntoken, ninp, nhid, nlayers, dropout = 0.5,
+    tie_weights = False):
+
+        super(RNNLM, self).__init__():
+        self.ntoken = ntoken
+        self.drop = nn.Dropout(dropout)
+        self.encoder = nn.Embedding(ntoken, ninp)
+
+        if rnn_type in ['LSTM', 'GRU']:
+            self.rnn = getattr(nn, rnn_type)(ninp, nhid, nlayers, dropout = dropout)
+        else:
+            try:
+                nonlinearity = {'RNN_TANH' : 'tanh', 'RNN_RELU' : 'relu'}[rnn_type]
+            except KeyError:
+                raise ValueError("""An invalid option for `--model` was supplied.
+                                    Options are ['LSTM', 'GRU', 'RNN_TANH' or 'RNN_RELU']""")
+            self.rnn = nn.RNN(ninp, nhid, nlayers, nonlinearity = nonlinearity, dropout = dropout)
+        self.decoder = nn.Linear(nhid, ntoken)
