9201cc9f884df99c898d9e5f17a32db0da532cb0
lennertjansen <lennertjansen95@gmail.com>
2020-05-14 15:56:09 +0200

    Fixed bug such that hidden states are adapted to last batch when dataset size is not a multiple of batch size.

--- project2/scripts/rnnlm_model.py
+++ project2/scripts/rnnlm_model.py
@@ -8,7 +8,7 @@ from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 # https://github.com/pytorch/examples/blob/master/word_language_model/model.py
 
 class RNNLM(nn.Module):
-    """Container module for RNN Language Model. Consists of an encoder,
+    """Container module for RNN Language Model. Consists of an embedder,
     a recurrent module, and a decoder."""
 
     def __init__(self, ntoken, ninp, nhid, nlayers, rnn_type = 'GRU', dropout = 0.2,
@@ -17,7 +17,7 @@ class RNNLM(nn.Module):
         super(RNNLM, self).__init__()
         self.ntoken = ntoken
         self.drop = nn.Dropout(dropout)
-        self.encoder = nn.Embedding(ntoken, ninp)
+        self.embedding = nn.Embedding(ntoken, ninp)
 
         self.rnn = nn.GRU(input_size = ninp, hidden_size = nhid,
                             num_layers = nlayers, bias = True, batch_first = True,
@@ -43,7 +43,7 @@ class RNNLM(nn.Module):
         if tie_weights:
             if nhid != ninp:
                 raise ValueError('When using the tied flag, nhid must be equal to emsize')
-            self.decoder.weight = self.encoder.weight
+            self.decoder.weight = self.embedding.weight
 
         # Call weight initialization function
         self.init_weights()
@@ -54,19 +54,19 @@ class RNNLM(nn.Module):
 
     def init_weights(self):
         initrange = 0.1
-        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.embedding.weight.data.uniform_(-initrange, initrange)
         self.decoder.bias.data.zero_()
         self.decoder.weight.data.uniform_(-initrange, initrange)
 
     def _embed_and_pack(self, input, lengths):
-        embedded = self.drop(self.encoder(input))
+        embedded = self.drop(self.embedding(input))
         packed = pack_padded_sequence(embedded, lengths, batch_first = True,
                                         enforce_sorted = False)
         return packed
 
     def forward(self, input, hidden, lengths):
 
-        embedding = self.drop(self.encoder(input))
+        embedding = self.drop(self.embedding(input))
         #packed = self._embed_and_pack(input, lengths)
 
         output, hidden = self.rnn(embedding, hidden)
