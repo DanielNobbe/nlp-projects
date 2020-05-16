f7ef5a4632815751d950816cec3d1eee68cd0344
lennertjansen <lennertjansen95@gmail.com>
2020-05-16 18:54:29 +0200

    Cleaned version of RNNLM code for both main and module. This version is used for training and testing on colab.

--- project2/scripts/rnnlm_model.py
+++ project2/scripts/rnnlm_model.py
@@ -12,8 +12,6 @@ class RNNLM(nn.Module):
     a recurrent module, and a decoder."""
 
     def __init__(self, ntoken, ninp, nhid, nlayers, rnn_type = 'GRU', dropout = 0.2):
-    # tie_weights = False):
-
         super(RNNLM, self).__init__()
         self.ntoken = ntoken
         self.drop = nn.Dropout(dropout)
@@ -22,41 +20,10 @@ class RNNLM(nn.Module):
         self.rnn = nn.GRU(input_size = ninp, hidden_size = nhid,
                             num_layers = nlayers, bias = True, batch_first = True,
                             dropout = dropout, bidirectional = False)
-
-        # if rnn_type in ['LSTM', 'GRU']:
-        #     self.rnn = getattr(nn, rnn_type)(ninp, nhid, nlayers, dropout = dropout)
-        # else:
-        #     try:
-        #         nonlinearity = {'RNN_TANH' : 'tanh', 'RNN_RELU' : 'relu'}[rnn_type]
-        #     except KeyError:
-        #         raise ValueError("""An invalid option for `--model` was supplied.
-        #                             Options are ['LSTM', 'GRU', 'RNN_TANH' or 'RNN_RELU']""")
-        #     self.rnn = nn.RNN(ninp, nhid, nlayers, nonlinearity = nonlinearity, dropout = dropout, batch_first = False)
         self.decoder = nn.Linear(nhid, ntoken)
-
-        # Optionally tie weights as in:
-        # "Using the Output Embedding to Improve Language Models" (Press & Wolf 2016)
-        # https://arxiv.org/abs/1608.05859
-        # and
-        # "Tying Word Vectors and Word Classifiers: A Loss Framework for Language Modeling" (Inan et al. 2016)
-        # https://arxiv.org/abs/1611.01462
-        # if tie_weights:
-        #     if nhid != ninp:
-        #         raise ValueError('When using the tied flag, nhid must be equal to emsize')
-        #     self.decoder.weight = self.embedding.weight
-
-        # Call weight initialization function
-        # self.init_weights()
-
         self.rnn_type = rnn_type
         self.nhid = nhid
         self.nlayers = nlayers
-
-    # def init_weights(self):
-    #     initrange = 0.1
-    #     self.embedding.weight.data.uniform_(-initrange, initrange)
-    #     self.decoder.bias.data.zero_()
-    #     self.decoder.weight.data.uniform_(-initrange, initrange)
 
     def _embed_and_pack(self, input, lengths):
         embedded = self.drop(self.embedding(input))
@@ -66,72 +33,17 @@ class RNNLM(nn.Module):
 
     def forward(self, input, hidden, lengths):
 
-        #print(input.
         batch_size = input.size(0)
-
-        # print('Input shape')
-        # print(input.shape)
-
         embedded = self.drop(self.embedding(input))
-
-        # print('Embedded shape')
-        # print(embedded.shape)
-
         packed = pack_padded_sequence(embedded, lengths, batch_first = True,
                                         enforce_sorted = False)
-
-
-
-        #embedding = self.drop(self.embedding(input))
-        #packed = self._embed_and_pack(input, lengths)
-
-        # print('Packed type')
-        # print(type(packed))
-        #
-        # print('Packed data shape')
-        # print(packed.data.shape)
-        #
-        # print('NU')
-
-        #output, hidden = self.rnn(embedded, hidden)
         output, hidden = self.rnn(packed, hidden)
-
-        # print('Type output')
-        # print(type(output))
-        #
-        # print('SHape output')
-        # print(output.data.shape)
-        #
-        # print('Type hidden')
-        # print(type(hidden))
-        #
-        # print('SHape hidden')
-        # print(hidden.shape)
-
-        #packed_decoder_input = _embed_and_pack(input, lengths)
-
-
-
-        # output = self.drop(output.data)
-        # decoded = self.decoder(output)
-        #
-        # unpacked, seq_lengths = pad_packed_sequence(decoded, batch_first = True)
-
-        #unpacked, lengths = pad_packed_sequence(decoded, batch_first = True)
-        #decoded = decoded.view(-1, self.ntoken) # TODO: either view(-1, seq_len, self.ntoken) or no resize
-
-        # How about this?
         unpacked, seq_lengths = pad_packed_sequence(output, batch_first = True)
-
         unpacked = self.drop(unpacked)
         decoded = self.decoder(unpacked)
-
-        #return F.log_softmax(decoded, dim = 1), hidden #TODO: use linear output and cross_entropy, or log_softmax and NLLLoss()
-        return decoded, hidden #TODO: shouldn't this have a softmax layer like Equation (1e) in project description
+        return decoded, hidden
 
     def init_hidden(self, bsz):
-        # weight = next(self.parameters())
-
         if self.rnn_type == 'LSTM':
             return (torch.zeros(self.nlayers, bsz, self.nhid),
                     torch.zeros(self.nlayers, bsz, self.nhid))
