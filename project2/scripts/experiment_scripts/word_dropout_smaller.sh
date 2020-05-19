6c1118cc0664850ebee10fa18cb53ec8b292b5fe
Daniel Laszlo <daniel.laszlo94@gmail.com>
2020-05-20 00:21:50 +0200

    Fix some output folders in word dropout experiments

--- project2/scripts/experiment_scripts/word_dropout_smaller.sh
+++ project2/scripts/experiment_scripts/word_dropout_smaller.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python vae.py --model_save_path word_dropout --word_dropout 0.33 --save_every 500
+python vae.py --model_save_path word_dropout_033 --word_dropout 0.33 --save_every 500
