6e8eda52fed8ccc8e907409ba66184079e8a54e1
chiefelectric <daniellnobbe@gmail.com>
2020-05-20 00:15:26 +0200

    Updates

--- project2/scripts/sample_sentences.py
+++ project2/scripts/sample_sentences.py
@@ -13,8 +13,9 @@ def sample_sentence(model, tokenizer, number):
     mean = torch.zeros(model.sampler.latent_size)
     std = torch.ones(model.sampler.latent_size)
     z = model.sampler(mean, std, 0) # Sample from standard Gaussian
+    # z = std
     # packed = model._embed_and_pack(z, lengths)
-
+    
     h = model.decoder.l2h(z)
     h = h.reshape(1, model.decoder.num_layers, model.decoder.hidden_size)
     h = h.transpose(0, 1)
@@ -28,18 +29,18 @@ def sample_sentence(model, tokenizer, number):
         word, h = model.decoder.rnn(word, h)
         # print("A: ", word.shape)
         token_index = model.decoded2vocab(word).argmax(dim=2).squeeze()
-        print("B: ", token_index)
+        # print("B: ", token_index)
         token = tokenizer.decode(token_index, skip_special_tokens=False)
         # token = token
         word = model.embedding(token_index).view(1, 1, -1)
         # print("Word: ", word.shape)
         # print("D: ", token)
         sentence.append(token)
-        print(token)
+        # print(token)
         # print("C: ", sentence)
 
     # decoded = model.decoder(z, packed)
-
+    sentence = (' ').join(sentence)
     print(sentence)
     return sentence
 
@@ -61,13 +62,13 @@ def main():
         freebits = 0, # Freebits value is the lambda value as described in Kingma et al. 
     )
 
-    model_load_name = Path('sentence_vae_FreeBits_0.001_4500.pt')
+    model_load_name = Path('MDRplusdropout_sentence_vae_MDR_5.0_4000.pt')
     models_path = Path('models')
 
     model_load_path = models_path / model_load_name 
 
     model.load_from(model_load_path)
-    print(model.state_dict)
+    # print(model.state_dict)
 
     sentence = sample_sentence(model, tokenizer, number=2)
     # print(sentences)
