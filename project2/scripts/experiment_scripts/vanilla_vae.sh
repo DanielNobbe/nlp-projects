4b4df4920222b13e387600cebacebf6367e9f45f
Daniel Laszlo <daniel.laszlo94@gmail.com>
2020-05-19 15:19:46 +0200

    Modify save interval in experiments, remove pdb breakpoint

--- project2/scripts/experiment_scripts/vanilla_vae.sh
+++ project2/scripts/experiment_scripts/vanilla_vae.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python vae.py --model_save_path vanilla
+python vae.py --model_save_path vanilla --save_every 500
--- project2/scripts/experiment_scripts/word_dropout.sh
+++ project2/scripts/experiment_scripts/word_dropout.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python vae.py --model_save_path word_dropout --word_dropout 0.66
+python vae.py --model_save_path word_dropout --word_dropout 0.66 --save_every 500
