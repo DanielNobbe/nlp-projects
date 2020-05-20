71f879718a01fec8a668c898550b11267b7c9173
Daniel Laszlo <daniel.laszlo94@gmail.com>
2020-05-20 04:36:42 +0200

    Freebits set parameter to 0.5

--- project2/scripts/experiment_scripts/freebits.sh
+++ project2/scripts/experiment_scripts/freebits.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python vae.py --model_save_path freebits --freebits 0.001 --save_every 500
+python vae.py --model_save_path freebits --freebits 0.5 --save_every 500
--- project2/scripts/experiment_scripts/word_dropout_freebits.sh
+++ project2/scripts/experiment_scripts/word_dropout_freebits.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python vae.py --model_save_path word_dropout_freebits --word_dropout 0.66 --freebits 0.001 --save_every 500
+python vae.py --model_save_path word_dropout_freebits --word_dropout 0.66 --freebits 0.5 --save_every 500
